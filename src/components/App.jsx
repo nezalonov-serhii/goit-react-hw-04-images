4cb283bb5aa8465ab7fe9ccec7194a0dd93fb759
Sergii <nezalonov.serhii@gmail.com>
2023-03-26 17:18:37 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { fetchImages } from 'api/fetch-api-gallery';
+import { useEffect, useState } from 'react';
 
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -8,10 +9,49 @@ export function App() {
   const [dataImages, setDataImages] = useState([]);
   const [page, setPage] = useState(1);
 
+  const [toggleLoader, setToggleLoader] = useState(false);
+  const [toggleButton, setToggleButton] = useState(true);
+
+  useEffect(() => {
+    if (!searchName) return;
+    setToggleLoader(true);
+
+    function getGallary() {
+      fetchImages(searchName, page)
+        .then(images => {
+          if (Math.ceil(images.total / 12) <= page) {
+            setToggleButton(false);
+          } else {
+            setToggleButton(true);
+          }
+
+          console.log('123');
+
+          setDataImages(prevImages => {
+            return [...prevImages, ...images.hits];
+          });
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+          setToggleLoader(false);
+        });
+    }
+
+    getGallary();
+  }, [page, searchName, setDataImages]);
+
+  function clickLoadMore() {
+    setPage(prevPage => {
+      return prevPage + 1;
+    });
+  }
+
   function onSubmitSearch(searchName) {
-    setSearchName(searchName);
-    setDataImages([]);
-    setPage(1);
+    if (searchName !== '') {
+      setSearchName(searchName);
+      setDataImages([]);
+      setPage(1);
+    }
   }
 
   return (
@@ -19,11 +59,11 @@ export function App() {
       <Searchbar onSubmit={onSubmitSearch} />
       {searchName && (
         <ImageGallery
-          searchName={searchName}
+          clickLoadMore={clickLoadMore}
           dataImages={dataImages}
-          setDataImages={setDataImages}
           page={page}
-          setPage={setPage}
+          toggleLoader={toggleLoader}
+          toggleButton={toggleButton}
         />
       )}
     </>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,53 +1,17 @@
-import { useEffect, useState } from 'react';
-
-import { fetchImages } from 'api/fetch-api-gallery';
 import { Button } from 'components/Button/ButtonLoad';
 import { Loader } from 'components/Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
 import { GalleryList, WrapGallary } from './ImageGallery.styled';
+import { useState } from 'react';
 
 export function ImageGallery(props) {
-  const { searchName, dataImages, page, setDataImages, setPage } = props;
+  const { dataImages, page, clickLoadMore, toggleLoader, toggleButton } = props;
 
-  const [toggleLoader, setToggleLoader] = useState(false);
-  const [toggleButton, setToggleButton] = useState(true);
   const [toggleModal, setToggleModal] = useState(false);
   const [largeImageUrl, setLargeImageUrl] = useState('');
   const [largeImageAlt, setLargeImageAlt] = useState('');
-
-  useEffect(() => {
-    function getGallary() {
-      fetchImages(searchName, page)
-        .then(images => {
-          if (images.hits.length === 0) return;
-          if (Math.ceil(images.total / 12) <= page) {
-            setToggleButton(false);
-          } else {
-            setToggleButton(true);
-          }
-
-          setDataImages(prevImages => {
-            return [...prevImages, ...images.hits];
-          });
-        })
-        .catch(error => console.log(error))
-        .finally(() => {
-          setToggleLoader(false);
-        });
-    }
-
-    if (searchName) {
-      getGallary();
-    }
-  }, [page, searchName, setDataImages]);
-
-  function clickLoadMore() {
-    setPage(prevPage => {
-      return prevPage + 1;
-    });
-  }
 
   function modalOpen(curentImage) {
     setLargeImageUrl(curentImage.largeImageURL);
@@ -66,21 +30,15 @@ export function ImageGallery(props) {
       {toggleLoader && page === 1 && (
         <Loader widthLoader={'200'} heightLoader={'200'} />
       )}
-      {dataImages[0] && (
+      {dataImages.length && (
         <WrapGallary>
-          {searchName && (
-            <>
-              <GalleryList>
-                <ImageGalleryItem images={dataImages} modalOpen={modalOpen} />
-              </GalleryList>
-              {toggleButton && (
-                <Button
-                  clickLoadMore={clickLoadMore}
-                  toggleLoader={toggleLoader}
-                />
-              )}
-            </>
+          <GalleryList>
+            <ImageGalleryItem images={dataImages} modalOpen={modalOpen} />
+          </GalleryList>
+          {toggleButton && (
+            <Button clickLoadMore={clickLoadMore} toggleLoader={toggleLoader} />
           )}
+
           {toggleModal && (
             <Modal
               url={largeImageUrl}
